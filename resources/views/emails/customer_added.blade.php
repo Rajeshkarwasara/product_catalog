19d1da180891776d093484a8c2a3c954c8012202
Rajeshkarwasara <rajeshjaat280@gmail.com>
2025-01-22 15:21:02 +0530

    user created and email send

--- resources/views/emails/customer_added.blade.php
+++ resources/views/emails/customer_added.blade.php
@@ -11,6 +11,7 @@
     <p><strong>Email:</strong> {{ $customer->email }}</p>
     <p><strong>Password:</strong> {{ $password }}</p>
     <p>Please log in and update your password at your earliest convenience.</p>
+    <p><a href="https://pcatalog.orbitnapp.com/login" style="color: blue; text-decoration: none;">Click here to log in</a></p>
 
 </body>
 </html>
