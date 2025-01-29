b37bd0194b6d41b3f6c6f8cb859cefca4cd6dd4d
RajeshMtoag <mtoag.php16@gmail.com>
2025-01-29 10:56:26 +0530

    Update product_view.blade.php

--- resources/views/content/order/product_view.blade.php
+++ resources/views/content/order/product_view.blade.php
@@ -1012,6 +1012,9 @@
                                     //form.submit();
                                     var formData = new FormData($("#addEditForm1")[0]);
                                     // alert(formData);
+                                    headers: {
+                                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                                    },
                                     formData.append('id', o_id);
                                     formData.append('status', o_value);
                                     var urlp = "{{ route('order.order_status') }}";
@@ -1020,9 +1023,6 @@
                                         type: "POST",
                                         enctype: 'multipart/form-data',
                                         url: urlp,
-                                        headers: {
-                                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                                        },
                                         data: formData,
                                         mimeType: "multipart/form-data",
                                         contentType: false,
